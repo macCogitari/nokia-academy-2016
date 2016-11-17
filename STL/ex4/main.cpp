76220d3f3cc4caf36c165d29c01e729022bf1beb
Maciej Trojnar <maciej.trojnar.ext@nokia.com>
2016-11-17 13:45:39 +0100

    ex4 list

--- STL/ex4/main.cpp
+++ STL/ex4/main.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
 
+#include <list>
+#include <numeric>
+
 
 int main(int argc, char *argv[])
 {
-    std::cout << "Hello World!" << std::endl;
+
+
+    std::list<int> numbers;
+
+    constexpr int range = 10000000;
+    constexpr int index = range/2;
+
+    numbers.resize(range);
+    std::iota(numbers.begin(), numbers.end(), 1);
+
+//    real	0m1.601s
+//    user	0m1.516s
+//    sys	0m0.080s
+
+//    numbers./
+    std::list<int>::iterator iter = numbers.begin();
+
+    for(int i=1; i<index; ++i){
+        ++iter;
+    }
+
+    std::cout <<  (*iter) << std::endl;
+
+//    real	0m1.618s
+//    user	0m1.516s
+//    sys	0m0.100s
+
     return 0;
 }
